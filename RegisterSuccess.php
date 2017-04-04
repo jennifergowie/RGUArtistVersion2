6d5db3bdcd1556ab77797f3a2405085d35123f2a
jennifergowie <0308182@rgu.ac.uk>
2017-04-05 00:54:18 +0100

    Add files via upload

---
+++ RegisterSuccess.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<!-- NB: add comments, sort formatting! -->
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>New Account Created</title>
+    <link rel="stylesheet" type="text/css" href="Resources/CSS/styleSheet.csstyleSheet.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Rakkas|Quicksand">
+
+</head>
+
+<body>
+
+
+<main>
+    <div class="container">
+        <h2>Your account has been created</h2>
+
+        <h4>Alternatively, click here to <a href="login.php">login</a></h4>
+    </div>
+
+</main>
+
+
+</body>
+
+</html>
